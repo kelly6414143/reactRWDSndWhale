8512fd96e7234227a4551464f9b3885a3f4a97c2
kelly6414143 <kelly6414143@gmail.com>
2020-08-30 00:19:20 +0800

    Updates

---
+++ precache-manifest.e3213ce84a860f1e74b0b5d29ba0a13b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a353ab123adf11d04b8d9e6d1c1fb1f",
+    "url": "/reactRWDSndWhale/index.html"
+  },
+  {
+    "revision": "1428064a5650a0c85c8d",
+    "url": "/reactRWDSndWhale/static/css/main.e0b98979.chunk.css"
+  },
+  {
+    "revision": "b0cd7b7e4ef0cb3cd6f1",
+    "url": "/reactRWDSndWhale/static/js/2.37290d7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactRWDSndWhale/static/js/2.37290d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1428064a5650a0c85c8d",
+    "url": "/reactRWDSndWhale/static/js/main.9f2d2ef5.chunk.js"
+  },
+  {
+    "revision": "66691158fce9b85da044",
+    "url": "/reactRWDSndWhale/static/js/runtime-main.dd95f892.js"
+  },
+  {
+    "revision": "ad75e952e7ed9b4cc6586c59a2e62545",
+    "url": "/reactRWDSndWhale/static/media/mv_img01@2x.ad75e952.png"
+  },
+  {
+    "revision": "f61b0ff24f196c38a78a6197f442c28b",
+    "url": "/reactRWDSndWhale/static/media/mv_img02@2x.f61b0ff2.png"
+  },
+  {
+    "revision": "738b9e061ff123069fb6f1232ee35114",
+    "url": "/reactRWDSndWhale/static/media/mv_img03@2x.738b9e06.png"
+  },
+  {
+    "revision": "0ac1eb4a12fbcec474f050f0a4dbd382",
+    "url": "/reactRWDSndWhale/static/media/mv_img04@2x.0ac1eb4a.png"
+  },
+  {
+    "revision": "ce8eaf28e66edf6fa10b0f1449b13fc8",
+    "url": "/reactRWDSndWhale/static/media/mv_ribbon@2x.ce8eaf28.png"
+  },
+  {
+    "revision": "3061453f07b6020765143d74a497d0c5",
+    "url": "/reactRWDSndWhale/static/media/mv_title01@2x.3061453f.png"
+  },
+  {
+    "revision": "905f8304fac3b7ab3931f0c2621b5553",
+    "url": "/reactRWDSndWhale/static/media/mv_title02@2x.905f8304.png"
+  },
+  {
+    "revision": "81a9b72bc2e88ff5df11531bd0149d61",
+    "url": "/reactRWDSndWhale/static/media/mv_titleBg@2x.81a9b72b.png"
+  },
+  {
+    "revision": "b720eb64c89cce039d303f3827db8581",
+    "url": "/reactRWDSndWhale/static/media/mv_titleSub@2x.b720eb64.png"
+  },
+  {
+    "revision": "0900f02602532e0f846ebdb627353f8a",
+    "url": "/reactRWDSndWhale/static/media/mv_wave_1.0900f026.png"
+  },
+  {
+    "revision": "396c8a5956d809e6f6861c72deab5b2a",
+    "url": "/reactRWDSndWhale/static/media/mv_wave_2.396c8a59.png"
+  },
+  {
+    "revision": "8e66154d6ac3ece9d76ca6fcaacc2c04",
+    "url": "/reactRWDSndWhale/static/media/mv_whale@2x.8e66154d.png"
+  }
+]);
